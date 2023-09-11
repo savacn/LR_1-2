d70cf8acdf82fdc2ba04582b6f461c0120347c08
Savely <112480298+savacn@users.noreply.github.com>
2023-09-12 00:25:50 +0300

    Add files via upload

---
+++ ,kjr 3 pflfxf 13.pas
@@ -0,0 +1,13 @@
+﻿program If12;
+var a, b, c:real;
+begin
+writeln('Введите числa a, b и c через пробел');22
+readln(a,b,c);
+if (a < b) and (a < c) then
+writeln('min = ',a)
+else
+if (b < a) and (b < c) then
+writeln('min = ',b)
+else
+writeln('min = ',c); 
+end.
