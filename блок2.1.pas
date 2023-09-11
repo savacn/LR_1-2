13e8401e458765119bff19abaadf60d9d7cd662c
Savely <112480298+savacn@users.noreply.github.com>
2023-09-11 22:34:19 +0300

    Add files via upload

---
+++ блок2.1.pas
@@ -0,0 +1,8 @@
+﻿program ssvs1;
+var n, k: integer;
+begin
+writeln('Введите любое число для проверки задания');
+readln(n);
+k:=n mod 10;
+write('Последней цифрой данного числа является - ', k);
+end.
