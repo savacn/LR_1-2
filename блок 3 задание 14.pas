0f2bc9f68aba4fbb871d17e8aaf119e82f034a49
Savely <112480298+savacn@users.noreply.github.com>
2023-09-12 00:32:50 +0300

    Add files via upload

---
+++ блок 3 задание 14.pas
@@ -0,0 +1,22 @@
+﻿program n3j3;
+
+var
+  a, b, c: integer;
+ 
+begin
+  Writeln('Введите три числа');
+  Readln(a, b, c);
+  if ((a > b) and (a > c) and (b > c)) Then
+    Write(a, ' ', b, ' ', c)
+  else
+  if ((b > a) and (b > c) and (a > c)) Then
+    Write(b, ' ', a, ' ', c)
+  else
+  if ((c > a) and (c > b) and (a > b)) Then
+    Write(c, ' ', a, ' ', b)
+  else
+  if ((b > a) and (b > c) and (c > a)) Then
+    Write(b, ' ', c, ' ', a)
+  else
+    Write( 'По возрастанию: ', c, ' ', b, ' ', a)
+end.
