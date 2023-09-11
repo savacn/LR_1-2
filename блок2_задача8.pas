535e458f789134e17761fa5533081141fbfec721
Savely <112480298+savacn@users.noreply.github.com>
2023-09-11 22:44:13 +0300

    Add files via upload

---
+++ блок2_задача8.pas
@@ -0,0 +1,12 @@
+﻿program ssvsdfs1;
+var tr, tr1, cht, cht1: integer;
+begin
+writeln('Введите любое трехзначное число');
+readln(tr);
+writeln('Введите любое четырёхзначное число');
+readln(cht);
+tr1:=tr div 100;
+writeln('Первой цифрой трехзначного числа является - ', tr1);
+cht1:=cht div 1000;
+writeln('Первой цифрой четырёхзначного числа является - ', cht1);
+end.
