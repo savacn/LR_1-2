c7ccbf2cfc9f2abd6ac17557dcd6201bf059cb56
Savely <112480298+savacn@users.noreply.github.com>
2023-09-11 23:50:19 +0300

    Add files via upload

---
+++ блок2_задание9.pas
@@ -0,0 +1,13 @@
+﻿program nsd3;
+var n, m: longint;
+    a: byte;
+begin
+readln(n);
+m := 0;
+  while n > 0 do begin
+        a := n mod 10;
+        m := m * 10 + a;
+        n := n div 10;
+                 end;
+        writeln(m);
+end.
