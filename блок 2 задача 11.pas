57c8c73a031a84f715f8cbfec20193da2c78a27d
Savely <112480298+savacn@users.noreply.github.com>
2023-09-12 00:13:43 +0300

    Add files via upload

---
+++ блок 2 задача 11.pas
@@ -0,0 +1,13 @@
+﻿Program dmem3;33
+var a, s, d, e, b, r: integer;
+begin
+write('Введите трехзначное число ');
+readln(a);
+s:= a div 100;
+d:= (a - s*100) div 10;
+e:= a - s*100 - d*10;
+b:= e*100 + d*10 + s;
+r:= a - b;
+writeln('Разность равна ', r)
+end.
+
