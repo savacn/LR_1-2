0f65f8444ec9d8c8a1a2a3cf260f3ef38be2dfac
Savely <112480298+savacn@users.noreply.github.com>
2023-09-12 00:36:02 +0300

    Add files via upload

---
+++ блок 3 задание 15.pas
@@ -0,0 +1,12 @@
+﻿Program sd3f;
+Var A, B, C, k:Integer;
+Begin
+k:=0;
+WriteLn('Введите числa A, B и C через пробел: ');
+ReadLn(A,B,C);
+If A > 0 Then k:=k + 1;
+If B > 0 Then k:=k + 1;
+If C > 0 Then k:=k + 1;
+WriteLn('Количество положительных чисел в исходном наборе: 22 
+',k);
+End.
