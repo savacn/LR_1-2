3e16832db54b56372c3a53102aadb0a804940e0a
Savely <112480298+savacn@users.noreply.github.com>
2023-09-21 11:51:02 +0300

    Update блок 3 задание 15.pas

--- блок 3 задание 15.pas
+++ блок 3 задание 15.pas
@@ -1,12 +1,11 @@
-﻿Program sd3f;
-Var A, B, C, k:Integer;
-Begin
+﻿program sd3f;
+var A, B, C, k:integer;
+begin
 k:=0;
-WriteLn('Введите числa A, B и C через пробел: ');
-ReadLn(A,B,C);
-If A > 0 Then k:=k + 1;
-If B > 0 Then k:=k + 1;
-If C > 0 Then k:=k + 1;
-WriteLn('Количество положительных чисел в исходном наборе: 22 
-',k);
-End.
+writeln('Введите числa A, B и C через пробел: ');
+readln(A,B,C);
+if A > 0 then k:=k + 1;
+if B > 0 then k:=k + 1;
+if C > 0 then k:=k + 1;
+writeln('Количество положительных чисел в исходном наборе: ',k);
+end.
