b8d676ed69f82498c5ad0d3f82749d5fec5c643c
Savely <112480298+savacn@users.noreply.github.com>
2023-09-21 12:37:52 +0300

    Add files via upload

---
+++ блок 4 задание 26.pas
@@ -0,0 +1,14 @@
+﻿program efv3g4;
+var a, b, c:integer;
+begin
+writeln('Введите a');
+readln(a);
+writeln('Введите b');
+readln(b);
+writeln('Введите c');
+readln(c);
+if (a<=(b+c)) and (b<=(a+c)) and (c<=(b+a)) then 
+                                   writeln('Треугольник с такими сторонами существует!')
+                                   else
+                                   writeln('Треугольник с такими сторонами НЕ существует!');
+end.
