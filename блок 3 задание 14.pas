2f11bf77e5e8ecfa8346ef589e7547e07a982de2
Savely <112480298+savacn@users.noreply.github.com>
2023-09-21 11:48:36 +0300

    Update блок 3 задание 14.pas

--- блок 3 задание 14.pas
+++ блок 3 задание 14.pas
@@ -1,22 +1,19 @@
 ﻿program n3j3;
-
-var
-  a, b, c: integer;
- 
+var a, b, c: integer;
 begin
-  Writeln('Введите три числа');
-  Readln(a, b, c);
-  if ((a > b) and (a > c) and (b > c)) Then
-    Write(a, ' ', b, ' ', c)
-  else
-  if ((b > a) and (b > c) and (a > c)) Then
-    Write(b, ' ', a, ' ', c)
-  else
-  if ((c > a) and (c > b) and (a > b)) Then
-    Write(c, ' ', a, ' ', b)
-  else
-  if ((b > a) and (b > c) and (c > a)) Then
-    Write(b, ' ', c, ' ', a)
-  else
-    Write( 'По возрастанию: ', c, ' ', b, ' ', a)
+writeln('Введите три числа');
+readln(a, b, c);
+if ((a > b) and (a > c) and (b > c)) then
+write(a, ' ', b, ' ', c)
+else
+if ((b > a) and (b > c) and (a > c)) then
+write(b, ' ', a, ' ', c)
+else
+if ((c > a) and (c > b) and (a > b)) then
+write(c, ' ', a, ' ', b)
+else
+if ((b > a) and (b > c) and (c > a)) then
+write(b, ' ', c, ' ', a)
+else
+write( 'По возрастанию: ', c, ' ', b, ' ', a);
 end.
