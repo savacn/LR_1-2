500d32500498ad7c381e4e20861d4092df86eb22
Savely <112480298+savacn@users.noreply.github.com>
2023-10-11 23:23:24 +0300

    Update блок 4 задание 26.pas

--- блок 4 задание 26.pas
+++ блок 4 задание 26.pas
@@ -1,14 +1,11 @@
-﻿program efv3g4;
-var a, b, c:integer;
+program z4_26;
+var 
+  a, b, c: real;
+
 begin
-writeln('Введите a');
-readln(a);
-writeln('Введите b');
-readln(b);
-writeln('Введите c');
-readln(c);
-if (a<=(b+c)) and (b<=(a+c)) and (c<=(b+a)) then 
-                                   writeln('Треугольник с такими сторонами существует!')
-                                   else
-                                   writeln('Треугольник с такими сторонами НЕ существует!');
+  writeln ('Введите стороны треугольника');
+  readln (a, b, c);
+  if (a+b > c) and (a+c > b) and (b+c > a) then 
+  writeln ('Такой треугольник существует!') else 
+  writeln ('Такой треугольник не существует!');
 end.
